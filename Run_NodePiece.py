73b92af30fc28fc805a95804a8183b0864488c05
brokenbeats <timoanthonijsz@gmail.com>
2025-07-10 20:08:59 +0200

    last update for matches NodePiece next

--- Run_NodePiece.py
+++ Run_NodePiece.py
@@ -1,8 +1,8 @@
 import rdflib
 from sentence_transformers import SentenceTransformer
-from modular_methods.embedding_utils import get_graph_embeddings_Node2vec
+from modular_methods.embedding_utils import get_graph_embeddings_PyKEEN
 from modular_methods.dedup_pipeline import deduplicate_graphs, save_matches
-from modular_methods.output_utils import build_final_result  
+from modular_methods.output_utils import build_final_result
 import time
 
 start_time = time.time()
@@ -12,8 +12,8 @@ g1 = rdflib.Graph()
 
 master_graph = rdflib.Graph()
 g1.parse("data/healthcare_graph_Main.ttl")
-#g2.parse("data/LLM_data/combined.ttl")
 
+#g2.parse("data/LLM_data/combined.ttl")
 master_graph.parse("data/master_data.ttl")
 phkg_graph = g1 + master_graph
 
@@ -23,10 +23,12 @@ for noise_level in noise_levels:
     start_time = time.time()
     g2 = rdflib.Graph()
     g2.parse(f"data/healthcare_graph_struct_{noise_level}.ttl")
-    # --- Graph embeddings (Node2Vec)
-    print("Computing graph embeddings...")
+
+    # --- Graph embeddings (DistMult)
+    print("Computing graph embeddings using DistMult...")
     combined_graph = phkg_graph + g2
-    graph_embeddings = get_graph_embeddings_Node2vec(combined_graph, dimensions=384)
+    graph_embeddings = get_graph_embeddings_PyKEEN(combined_graph, model ="DistMult", dimensions=384, num_epochs=60)
+
     # --- Run deduplication for multiple alpha values
     alpha_values = [0.0, 0.2, 0.35, 0.5, 0.65, 0.8]  # Change as needed
     for alpha in alpha_values:
@@ -38,7 +40,7 @@ for noise_level in noise_levels:
             use_hybrid=True,
             alpha=alpha,
             text_dim=384,
-            threshold=0.5,
+            threshold=0.6,
             top_k=5,
             filter_literals=True,
         )
@@ -53,13 +55,15 @@ for noise_level in noise_levels:
             graph2_name="g2"
         )
 
-        output_path = f"matches_struct_{noise_level}/HybridNode2VecLow_alpha_{alpha}.json"
+        # --- Save as JSON
+        output_path = f"matches_struct_{noise_level}/HybridDistMult_alpha_{alpha}.json"
         save_matches(final_result, output_path)
         print(f"Saved matches to {output_path}")
+
     end_time = time.time()
     runtime = end_time - start_time
     print(f"Total runtime: {runtime:.2f} seconds")
 
     # Save runtime to file
     with open("runtimes.txt", "a") as f:
-        f.write(f"Run with model = Node2Vec and alpha={alpha_values} took {runtime:.2f} seconds with noise being {noise_level}\n")
+        f.write(f"Run with model = DistMult and alpha={alpha_values} took {runtime:.2f} seconds with noise being {noise_level}\n")
